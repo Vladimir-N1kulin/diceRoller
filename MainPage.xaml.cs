104cf0df6da5159d0594966f838713ca06ae60b1
FoxsEyes <109696639+FoxsEyes@users.noreply.github.com>
2024-10-19 15:02:54 -0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace diceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            int roll = random.Next(1, 7);
+
+            switch (roll)
+            {
+                case 1:
+                    DiceImg.Source = "dice1.png";
+                    break;
+                case 2:
+                    DiceImg.Source = "dice2.png";
+                    break;
+                case 3:
+                    DiceImg.Source = "dice3.png";
+                    break;
+                case 4:
+                    DiceImg.Source = "dice4.png";
+                    break;
+                case 5:
+                    DiceImg.Source = "dice5.png";
+                    break;
+                case 6:
+                    DiceImg.Source = "dice6.png";
+                    break;
+
+
+
+            }
+        }
+
+    }
+}
